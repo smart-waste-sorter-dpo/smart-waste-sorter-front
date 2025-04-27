38155a63730b932925d9132031cd7a23ce0651c2
Arina Topor <arinatopor@abanking.ru>
2025-04-27 15:36:20 +0500

    fix upload

--- waste-recognition/src/app/scan/component/scan.component.ts
+++ waste-recognition/src/app/scan/component/scan.component.ts
@@ -1,31 +1,39 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { Router } from '@angular/router';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import {
   IonButton,
   IonContent,
-  NavController,
+  Platform,
+  IonSpinner,
 } from '@ionic/angular/standalone';
-import { catchError, EMPTY, from, of, switchMap } from 'rxjs';
-import { Classify } from '../interfaces/classify.interface';
+import { catchError, EMPTY, from, switchMap } from 'rxjs';
+import { HTTP } from '@awesome-cordova-plugins/http/ngx';
 
 @Component({
   selector: 'app-onboarding',
   templateUrl: './scan.component.html',
-  imports: [IonContent, IonButton, HttpClientModule],
+  imports: [IonContent, IonButton, HttpClientModule, IonSpinner],
+  providers: [HTTP],
   styleUrl: './styles/scan.component.scss',
 })
 export class ScanComponent {
-  protected loading: WritableSignal<boolean> = signal(false);
+  public loading: WritableSignal<boolean> = signal(false);
   private _imageUrl?: string = '';
   private _httpClient: HttpClient = inject(HttpClient);
   private _router: Router = inject(Router);
+  private nativeHttp: HTTP = inject(HTTP);
+  private platform: Platform = inject(Platform);
 
   openCamera() {
+    this.loading.set(true);
+
     from(
       Camera.getPhoto({
         quality: 90,
+        width: 400,
         allowEditing: false,
         resultType: CameraResultType.Uri,
         source: CameraSource.Camera,
@@ -37,32 +45,66 @@ export class ScanComponent {
           return this.uploadImage(photo);
         }),
         catchError((err) => {
-          console.log(err);
+          console.error(err);
           this._router.navigate(['/error']);
           return EMPTY;
         })
       )
       .subscribe((response: any) => {
         console.log(response);
+        let result: any;
+
+        if (response.data) {
+          result = JSON.parse(response.data);
+        } else {
+          result = response;
+        }
+        this.loading.set(false);
         this._router.navigate(['/success'], {
           queryParams: {
-            class: response.class,
+            class: result.class,
           },
         });
       });
   }
 
   uploadImage(photo: any) {
-    const formData = new FormData();
-    return from(fetch(photo.webPath!).then((r) => r.blob())).pipe(
-      switchMap((imageBlob: Blob) => {
-        formData.append('file', imageBlob, 'scan-image.jpg');
-        return this._httpClient.post('/api/v1/wastes/classify/', formData);
-      }),
-      catchError(() => {
-        this._router.navigate(['/error']);
-        return EMPTY;
-      })
-    );
+    const filePath = photo.path || photo.webPath; // берем путь к файлу
+
+    if (!filePath) {
+      console.error('No file path found in photo object');
+      return EMPTY;
+    }
+
+    if (this.platform.is('capacitor') || this.platform.is('cordova')) {
+      return from(
+        this.nativeHttp.uploadFile(
+          'https://waste-api.vstrechya.space/api/v1/wastes/classify/',
+          {},
+          { 'Content-Type': 'multipart/form-data' },
+          filePath,
+          'file'
+        )
+      ).pipe(
+        catchError((err) => {
+          console.error(err);
+          return EMPTY;
+        })
+      );
+    } else {
+      // Браузер: сначала превращаем в FormData
+      return from(fetch(photo.webPath!).then((r) => r.blob())).pipe(
+        switchMap((imageBlob: Blob) => {
+          const formData = new FormData();
+          formData.append('file', imageBlob, 'scan-image.jpeg');
+
+          return this._httpClient.post('/api/v1/wastes/classify/', formData);
+        }),
+        catchError((err) => {
+          console.error(err);
+          return EMPTY;
+        })
+      );
+    }
   }
 }
--- waste-recognition/src/environments/environment.prod.ts
+++ waste-recognition/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://waste-api.vstrechya.space/api/v1',
 };
